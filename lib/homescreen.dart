c5a8bcd79bbee58eee1f2ee3f1373e5628046ae0
Arjun Narayanan <arjun14081999@gmail.com>
2020-05-15 19:54:20 +0530

    states added

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -16,7 +17,9 @@ class _HomeScreenState extends State<HomeScreen> {
   var deceased = '0';
   var lastUpdatedTime;
   var lastUpdatedTimeDate;
-  bool loading = true;
+  bool loading = false;
+  List statesInfo;
+  var length;
 
   @override
   void initState() {
@@ -26,11 +29,11 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Future getData() async {
+    loading = true;
     http.Response response =
         await http.get('https://api.covid19india.org/data.json');
     String data = response.body;
     body = (jsonDecode(data));
-    print(body['statewise'][0]);
     setState(() {
       confirmed = body['statewise'][0]['confirmed'];
       active = body['statewise'][0]['active'];
@@ -39,14 +42,17 @@ class _HomeScreenState extends State<HomeScreen> {
       lastUpdatedTime = body['statewise'][0]['lastupdatedtime'];
       lastUpdatedTimeDate = lastUpdatedTime.toString().substring(0, 5) +
           lastUpdatedTime.toString().substring(10, 16);
+      statesInfo = body['statewise'];
+      statesInfo.removeAt(0);
+      length = statesInfo.length;
       loading = false;
     });
-
+    print(statesInfo[1]);
   }
 
   @override
   Widget build(BuildContext context) {
-     return Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text(
           'COVID19 INDIA',
@@ -55,47 +61,114 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
       ),
-      body: Container(
-        child: Padding(
-          padding: const EdgeInsets.all(10.0),
-          child: Column(
-            children: <Widget>[
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                //child: Text('temp'),
-                child: Text('Last updated on: $lastUpdatedTimeDate IST'),
-              ),
-              Row(
+      body: ModalProgressHUD(
+        inAsyncCall: loading,
+        child: SafeArea(
+          child: Container(
+            child: Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Column(
                 children: <Widget>[
-                  CasesNumber(
-                    status: 'Confirmed',
-                    colour: Colors.red,
-                    number: confirmed,
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    //child: Text('temp'),
+                    child: Text('Last updated on: $lastUpdatedTimeDate IST'),
                   ),
-                  CasesNumber(
-                    status: 'Active',
-                    colour: Colors.blue,
-                    number: active,
+                  Row(
+                    children: <Widget>[
+                      CasesNumber(
+                        status: 'Confirmed',
+                        colour: Colors.red,
+                        number: confirmed,
+                      ),
+                      CasesNumber(
+                        status: 'Active',
+                        colour: Colors.blue,
+                        number: active,
+                      ),
+                      CasesNumber(
+                        status: 'Recovered',
+                        colour: Colors.green,
+                        number: recovered,
+                      ),
+                      CasesNumber(
+                        status: 'Deceased',
+                        colour: Colors.grey,
+                        number: deceased,
+                      ),
+                    ],
                   ),
-                  CasesNumber(
-                    status: 'Recovered',
-                    colour: Colors.green,
-                    number: recovered,
+                  SizedBox(
+                    height: 12,
                   ),
-                  CasesNumber(
-                    status: 'Deceased',
-                    colour: Colors.grey,
-                    number: deceased,
+                  Row(
+                    children: <Widget>[
+                      Expanded(flex: 2,child: Text('State/UT',),),
+                      Expanded(flex: 1,child: Text('confirmed',style: TextStyle(fontSize: 12),textAlign: TextAlign.center,),),
+                      Expanded(flex: 1,child: Text('active',style: TextStyle(fontSize: 12),textAlign: TextAlign.center,),),
+                      Expanded(flex: 1,child: Text('recovered',style: TextStyle(fontSize: 12),textAlign: TextAlign.center,),),
+                      Expanded(flex: 1,child: Text('deaths',style: TextStyle(fontSize: 12),textAlign: TextAlign.center,),),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 12,
+                  ),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: length,
+                        itemBuilder: (context, int index) {
+                          return Row(
+                            children: <Widget>[
+                              StatesCases(statesInfo: statesInfo[index]['state'],flex: 2,alignment: CrossAxisAlignment.start,),
+                              StatesCases(statesInfo: statesInfo[index]['confirmed'],flex: 1,alignment: CrossAxisAlignment.end,),
+                              StatesCases(statesInfo: statesInfo[index]['active'],flex: 1,alignment: CrossAxisAlignment.end,),
+                              StatesCases(statesInfo: statesInfo[index]['recovered'],flex: 1,alignment: CrossAxisAlignment.end,),
+                              StatesCases(statesInfo: statesInfo[index]['deaths'],flex: 1,alignment: CrossAxisAlignment.end,),
+                            ],
+                          );
+                        }),
                   ),
                 ],
               ),
-            ],
+            ),
           ),
         ),
       ),
-    ) ;
+    );
   }
 }
+
+class StatesCases extends StatelessWidget {
+
+  StatesCases({this.statesInfo,this.flex,this.alignment});
+  final  statesInfo;
+  final flex ;
+  final alignment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Column(
+        crossAxisAlignment: alignment,
+        children: <Widget>[
+          Container(
+            //color: Color(0xFF161625),
+            child: Text(
+              statesInfo,
+              style: TextStyle(
+                fontSize: 15,
+              ),
+            ),
+          ),
+          SizedBox(height: 12,),
+        ],
+      ),
+      flex: flex,
+    );
+  }
+}
+
+
 
 class CasesNumber extends StatelessWidget {
   CasesNumber({this.status, this.colour, @required this.number});
